8a7b5ea22fe4f4df6dcaa23140d64736e872ed04
Umar Niazi <uhkniazi@gmail.com>
2017-05-04 14:45:25 +0100

    script done check point

--- BayesEstimation/marathonTimes.R
+++ BayesEstimation/marathonTimes.R
@@ -63,10 +63,8 @@ Sigma2 = seq(500, 9000, by=10)
 
 ## define a log posterior function
 lp = function(theta, data){
-  # we define the sigma on a log scale as optimizers work better
-  # if scale parameters are well behaved
-  s = exp(theta['sigma2'])
-  m = theta['mu']
+  s = theta[2]
+  m = theta[1]
   d = data$time # data from marathon times
   log.lik = sum(dnorm(d, m, sqrt(s), log=T))
   log.prior = 1
@@ -76,18 +74,19 @@ lp = function(theta, data){
 
 # sanity check for function
 # choose a starting value
-start = c('mu'=Mu[1], 'sigma2'=log(Sigma2[1]))
+start = c('mu'=Mu[1], 'sigma2'=Sigma2[1])
 lData = list('time'=time)
 lp(start, lData)
-
-op = optim(start, lf, control = list(fnscale = -1), data=X, hessian = T)
-exp(op$par[2])
+# draw the contour plot 
+mycontour(lp, c(220, 330, 500, 9000), lData)
+# how do the simulated values earlier fit in this contour plot
+points(sim.1$mu, sim.1$var, pch=20)
 
 ## calculate the log posterior at each grid point 
 lm = matrix(NA, nrow=length(Mu), ncol = length(Sigma2))
 for (r in seq_along(Mu)){
   for (c in seq_along(Sigma2)){
-    s = c('mu'=Mu[r], 'sigma2'=log(Sigma2[c]))
+    s = c('mu'=Mu[r], 'sigma2'=Sigma2[c])
     lm[r, c] = lp(s, lData)
   }
 }
@@ -99,14 +98,79 @@ lm = exp(lm)
 # set total prob of grid to 1
 lm = lm/sum(colSums(lm))
 
-## plot contour
-contour(Mu, Sigma, lm, xlim=c(9, 10.5), ylim=c(2.2, 3.5))
-
 ## take samples from this distribution
-pSigma = colSums(lm)
+pSigma2 = colSums(lm)
 pMu = rowSums(lm)
 
-sigSample = runif(1000, -0.05, 0.05) +  sample(size = 1000, Sigma, replace = T, prob = pSigma)
-muSample = runif(1000, -0.05, 0.05) + sample(size = 1000, Mu, replace = T, prob = pMu)
-contour(Mu, Sigma, (lm)/max(lm),  levels = c(0, 0.5, 0.3, 0.1, 0.01, 0.001, 0.0001))
-points(muSample, sigSample, col=1, pch=20, cex=0.5)
+## add a uniform jitter centered at zero and half of grid spacing to make distribution continuous
+sig2Sample = runif(1000, -1*10/2, 10/2) +  sample(size = 1000, Sigma2, replace = T, prob = pSigma2)
+# for each sampled value of sig2Sample, get the posterior mu, conditioned on sig2Sample
+muSample = rnorm(1000, mean(time), sqrt(sig2Sample)/sqrt(length(time)))
+## how do they fit on the contour plot
+points(muSample, sig2Sample, pch=20, col=2)
+## pretty close
+quantile(muSample, c(0.025, 0.975))
+quantile(sqrt(sig2Sample), c(0.025, 0.975))
+quantile(sig2Sample, c(0.025, 0.975))
+
+# take a sample directly from the grid for mean
+muSample2 = runif(1000, -1*0.5/2, 0.5/2) + sample(size = 1000, Mu, replace = T, prob = pMu)
+quantile(muSample2, c(0.025, 0.975))
+
+################################################################
+#### try an optimization based approach
+## define a log posterior function
+lp2 = function(theta, data){
+  # we define the sigma on a log scale as optimizers work better
+  # if scale parameters are well behaved
+  s = exp(theta[2])
+  m = theta[1]
+  d = data$time # data from marathon times
+  log.lik = sum(dnorm(d, m, s, log=T))
+  log.prior = 1
+  log.post = log.lik + log.prior
+  return(log.post)
+}
+
+# sanity check for function
+# choose a starting value
+start = c('mu'=mean(time), 'sigma'=log(var(time)))
+lData = list('time'=time)
+lp2(start, lData)
+
+op = optim(start, lp2, control = list(fnscale = -1), data=lData)
+op$par
+exp(op$par[2])
+
+## try the laplace function from LearnBayes
+fit = laplace(lp2, start, lData)
+fit
+se = sqrt(diag(fit$var))
+se
+fit$mode+1.96*se
+fit$mode-1.96*se
+
+## lets take a sample from this
+sigSample.op = rnorm(1000, fit$mode['sigma'], se['sigma'])
+muSample.op = rnorm(1000, mean(time), exp(sigSample.op)/sqrt(length(time)))
+muSample2.op = rnorm(1000, fit$mode['mu'], se['mu'])
+
+##### lets plot all the various approximations together
+mycontour(lp, c(220, 330, 500, 9000), lData, xlab='Posterior Mean', ylab='Posterior Variance')
+# how do the simulated values earlier fit in this contour plot
+points(sim.1$mu, sim.1$var, pch=20, col='black')
+points(muSample, sig2Sample, pch=20, col='red')
+points(muSample.op, exp(sigSample.op)^2, pch=20, col='blue')
+
+par(mfrow=c(1,2))
+plot(density(sim.1$mu), col=1, type='l', main='Posterior Mean Density')
+lines(density(muSample), col='red')
+lines(density(muSample.op), col='blue')
+lines(density(muSample2.op), col='green')
+lines(density(muSample2), col='darkgreen')
+
+plot(density(sim.1$var), col=1, type='l', main='Posterior Variance Density')
+lines(density(sig2Sample), col='red')
+lines(density(exp(sigSample.op)^2), col='blue')
+
+
