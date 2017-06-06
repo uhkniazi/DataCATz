02dbcb569c140c05b3516ee5a97c55a813d973ae
Umar Niazi <uhkniazi@gmail.com>
2017-06-06 13:30:55 +0100

    section for binomial regression written

--- BayesRegression/binomialRegression.R
+++ BayesRegression/binomialRegression.R
@@ -9,6 +9,8 @@
 library(lattice)
 library(lme4)
 library(mlmRev)
+library(car)
+library(LearnBayes)
 
 data("Contraception")
 
@@ -25,4 +27,53 @@ xyplot(ifelse(use == 'Y', 1, 0) ~ age|urban, data = Contraception, groups = livc
 ## 2- middle age range women are more likely to use contraception
 ## 3- Urban women are more likely to use contraception
 ## 4- women with 0 childeren are less likely to use contraception.
+## 5- women who have children are less different from each other
+
+## using point 5, merge the groups 1, 2, and 3 in the variable livch
+Contraception$children = factor(Contraception$livch != 0, labels=c('N', 'Y'))
+
+str(Contraception)
+
+xyplot(ifelse(use == 'Y', 1, 0) ~ age|urban, data = Contraception, groups = children, lty=1:2, col=1,
+       type=c('g', 'smooth'), key = list(text=list(levels(Contraception$children)), lines=list(lty=1:2),
+                                         columns=2, cex=0.8),
+       xlab='Age', ylab='Contraception Use', main=list(label='Contraception use Given Urban', cex=0.8))
+
+## we should perhaps also use an interaction term between age and number of children, as the slopes and interceptrs
+## in cases with children are different to those without children.
+
+## lets fit a model using the binomial glm without random effects
+fit.1 = glm(use ~ age + I(age^2) + urban + children, data=Contraception, family = binomial(link='logit'))
+summary(fit.1)
+
+
+logit.inv = function(p) {exp(p)/(exp(p)+1) }
+
+## lets write a custom glm using a bayesian approach
+## write the log posterior function
+mylogpost = function(theta, data){
+  ## parameters to track/estimate
+  betas = theta # vector of betas i.e. regression coefficients for population
+  ## data
+  resp = data$resp # resp
+  mModMatrix = data$mModMatrix
+  
+  # calculate fitted value
+  iFitted = mModMatrix %*% betas
+  # using logit link so use inverse logit
+  iFitted = logit.inv(iFitted)
+  # write the priors and likelihood 
+  lp = dcauchy(betas[1], 0, 10, log=T) + sum(dcauchy(betas[-1], 0, 10, log=T))
+  lik = sum(dbinom(resp, 1, iFitted, log=T))
+  val = lik + lp
+  return(val)
+}
+
+lData = list(resp=ifelse(Contraception$use == 'N', 0, 1), mModMatrix=model.matrix(use ~ age + I(age^2) + urban + children, data=Contraception))
+start = c(rep(0, times=ncol(lData$mModMatrix)))
+
+mylogpost(start, lData)
+
+fit.2 = laplace(mylogpost, start, lData)
+se = sqrt(diag(fit.2$var))
 
