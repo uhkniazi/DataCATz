6819f1f503b1b4599bdb290de2fccc03de6e5f5b
uhkniazi <uhkniazi@gmail.com>
2017-07-19 20:25:50 +0100

    done the figures

--- sampling/hypothesisTests.R
+++ sampling/hypothesisTests.R
@@ -17,7 +17,7 @@ m3 = function(th) dbeta(th, 5, 1, log = T)
 ## define an array that represents number of models in our parameter space
 ## each index has a prior weight/probability of being selected
 ## this can be thought of coming from a categorical distribution 
-mix.prior = c(m1=10/11 *(1-1e-6),m2= 1/11 *(1-1e-6),m3= 1e-6)
+mix.prior = c(m1=10/15 ,m2= 4/15 ,m3= 1/15)
 
 ## define a joint prior space for these 3 models
 g.mix = function(theta) {
@@ -34,7 +34,8 @@ curve(g.mix, 0, 1, main='Joint Prior Mixture of 3 models')
 # we observe data from a machine, how many bad do we see
 data = rbinom(20, 1, 1/6)
 b = sum(data)
-data = c(fail=length(data)-b, success=b)
+data = c(good=length(data)-b, bad=b)
+data = c(good=0, bad=20)
 
 ############################
 ## try using log posterior function
@@ -45,23 +46,22 @@ logit.inv = function(p) {exp(p)/(exp(p)+1) }
 mylogpost_m1 = function(theta, data){
   ## theta contains parameters we wish to track
   th = logit.inv(theta['theta'])
-  suc = data['success']
-  fail = data['fail']
+  suc = data['bad']
+  fail = data['good']
   
   # define likelihood function
   lf = function(s, f, t) return(dbinom(s, s+f, t, log=T))
   
-  if (th >=0.17) return(-Inf)
   # calculate log posterior
-  val = lf(suc, fail, th) + m1(th) #dunif(th, 0, 0.17, log=T)
+  val = lf(suc, fail, th) + m1(th) 
   return(val)
 }
 
 mylogpost_m2 = function(theta, data){
   ## theta contains parameters we wish to track
   th = logit.inv(theta['theta'])
-  suc = data['success']
-  fail = data['fail']
+  suc = data['bad']
+  fail = data['good']
   
   # define likelihood function
   lf = function(s, f, t) return(dbinom(s, s+f, t, log=T))
@@ -74,8 +74,8 @@ mylogpost_m2 = function(theta, data){
 mylogpost_m3 = function(theta, data){
   ## theta contains parameters we wish to track
   th = logit.inv(theta['theta'])
-  suc = data['success']
-  fail = data['fail']
+  suc = data['bad']
+  fail = data['good']
   
   # define likelihood function
   lf = function(s, f, t) return(dbinom(s, s+f, t, log=T))
@@ -114,7 +114,7 @@ BFm1 = exp(fit_m1$int) / exp(fit_m2$int)
 BFm3 = exp(fit_m3$int) / exp(fit_m2$int)
 
 # posterior prob for the models
-mix.post = c(m1=0, m2=0, m3=0)
+mix.post = mix.prior
 mix.post[1] = exp(fit_m1$int) * mix.prior[1] / (exp(fit_m1$int) * mix.prior[1] + exp(fit_m2$int) * mix.prior[2]
                                                 + exp(fit_m3$int) * mix.prior[3])
 
@@ -125,3 +125,31 @@ mix.post[3] = exp(fit_m3$int) * mix.prior[3] / (exp(fit_m1$int) * mix.prior[1] +
                                                 + exp(fit_m3$int) * mix.prior[3])
 
 mix.post
+
+ivData = c(0, 0, 1, 1, 0, 1, rep(1, times=10), rep(0, times=5), rep(1, times=3))
+
+lMixs = lapply((3:length(ivData)), function(x){
+  data = ivData[1:x]
+  b = sum(data)
+  data = c(good=length(data)-b, bad=b)
+  start = c(theta=logit(0.5))
+  fit_m1 = laplace(mylogpost_m1, start, data)
+  fit_m2 = laplace(mylogpost_m2, start, data)
+  fit_m3 = laplace(mylogpost_m3, start, data)
+  mix.post = mix.prior
+  mix.post[1] = exp(fit_m1$int) * mix.prior[1] / (exp(fit_m1$int) * mix.prior[1] + exp(fit_m2$int) * mix.prior[2]
+                                                  + exp(fit_m3$int) * mix.prior[3])
+  
+  mix.post[2] = exp(fit_m2$int) * mix.prior[2] / (exp(fit_m1$int) * mix.prior[1] + exp(fit_m2$int) * mix.prior[2]
+                                                  + exp(fit_m3$int) * mix.prior[3])
+  
+  mix.post[3] = exp(fit_m3$int) * mix.prior[3] / (exp(fit_m1$int) * mix.prior[1] + exp(fit_m2$int) * mix.prior[2]
+                                                  + exp(fit_m3$int) * mix.prior[3])
+  return(mix.post)
+})
+
+mMixs = do.call(rbind, lMixs)
+matplot(mMixs, type='l', lty=1, lwd=2, xaxt='n', main='Posterior probabilities for the 3 hypotheses')
+axis(1, at = 1:nrow(mMixs), labels = 3:length(ivData), cex.axis=0.7)
+legend('topright', legend = c('M1', 'M2', 'M3'), fill=c('black', 'red', 'green'))
+points(rep(1, times=3), mix.prior, col= c('black', 'red', 'green'), pch=20)
