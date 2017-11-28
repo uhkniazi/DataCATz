40ecff14e2fe4776d27c77b4ae3da6e0efd4a598
Umar Niazi <uhkniazi@gmail.com>
2017-11-28 15:00:08 +0000

    added a one level model that can be implemented using a conjugate analysis

--- hierarchicalBinomial/oneLevelBinomialHierarchicalModel.stan
+++ hierarchicalBinomial/oneLevelBinomialHierarchicalModel.stan
@@ -1,22 +1,23 @@
 data {
   int<lower=1> Ntotal; // number of observations
-  int<lower=1> NgroupsLvl1; // number of levels for the level 1 of hierarchy - population level
-  int<lower=1, upper=NgroupsLvl1> NgroupsLvl2Map[Ntotal]; // mapping variable to map each value at lvl 2 of hierarchy to lvl 1 variables
+  int<lower=Ntotal> Ngroups1; // number of levels for the level 1 of hierarchy - group level
+  int<lower=1, upper=Ngroups1> NgroupsMap[Ntotal]; // mapping variable to map data points to estimated parameters
   int y[Ntotal]; // number of success
-  int<lower=1> N[Ntotal]; // total trials, at least one trial
+  int<lower=1> N[Ntotal]; // total trials, at least one trial required
 }
 
 parameters { // the parameters to track
-  real<lower=0, upper=1> theta[Ntotal];
-  real<lower=0, upper=1> alpha1[NgroupsLvl1];
-  real<lower=0> beta1[NgroupsLvl1];
+  real<lower=0, upper=1> theta[Ntotal];   // parameter of interest for level 1 of hierarachy
+  real<lower=0, upper=1> alpha; // hyper-parameter or population level parameter  
+  real<lower=0> beta; // hyper-parameter 
 }
 // transformed parameters {
 // 
 // }
 model {
   // distribution for level paremeter - used in the likelihood
-  theta ~ beta(alpha1[NgroupsLvl2Map], beta1[NgroupsLvl2Map]);
+  theta ~ beta(alpha, beta);
   ////// likelihood
-  y ~ binomial(N, theta);
+  y ~ binomial(N, theta[NgroupsMap]);
 }
+
--- hierarchicalBinomial/twoLevelBinomialHierarchicalModel.R
+++ hierarchicalBinomial/twoLevelBinomialHierarchicalModel.R
@@ -89,6 +89,240 @@ dfData[c(573, 428),]
 ## data influences the shrinkage and it is less than in previous case
 hist(mPlayers[,'Andrew McCutchen'] - mPlayers[,'Brett Jackson'])
 
+##############################################################################
+### merge the lvl 2 data i.e. players, as we are only interested in 
+### positions, trying a different parameterization of the model
+d1 = tapply(dfData$Hits, dfData$PriPos, sum)
+d2 = tapply(dfData$AtBats, dfData$PriPos, sum)
+dfData.2 = data.frame(PriPos = factor(levels(dfData$PriPos)), Hits=d1, AtBats=d2)
+
+stanDso.2 = rstan::stan_model(file='oneLevelBinomialHierarchicalModel.stan')
+
+## set up starting data
+lStanData = list(Ntotal=nrow(dfData.2), Ngroups1=nlevels(dfData.2$PriPos), 
+                 NgroupsMap=as.numeric(dfData.2$PriPos),
+                 y=dfData.2$Hits,
+                 N=dfData.2$AtBats)
+
+fit.stan.2 = sampling(stanDso.2, data=lStanData, iter=1000, chains=2,
+                    cores=2)#, control=list(adapt_delta=0.99, max_treedepth = 15))
+print(fit.stan.2, digits=3)
+
+mPositions.2 = extract(fit.stan.2)$theta
+dim(mPositions.2)
+colnames(mPositions.2) = levels(dfData.2$PriPos)
+
+## figure 9.14 from [1]
+par(mfrow=c(2,2))
+hist(mPositions.2[,'Pitcher'])
+hist(mPositions.2[,'Pitcher'] - mPositions.2[,'Catcher'])
+plot(mPositions.2[,'Pitcher'], mPositions.2[,'Catcher'], pch=20)
+hist(mPositions.2[,'Catcher'])
+
+## compare the 2 models
+par(mfrow=c(1,2))
+plot(colMeans(mPositions), colMeans(mPositions.2), pch=20, xlim=c(0.23, 0.27), ylim=c(0.23, 0.27))
+
+########### try a conjugate approach 
+## get alpha and beta parameters for the beta distribution from the data
+## see gelman p 583
+getalphabeta = function(m, v){
+  al.be = (m * (1-m) / v) - 1
+  al = al.be * m
+  be = al.be * (1-m)
+  return(c(alpha=al, beta=be))
+}
+
+getFittedTheta = function(param){
+  iAlpha = param[1];
+  iBeta = param[2];
+  iSuc = param[3];
+  iFail = param[4];
+  return(rbeta(1000, iSuc+iAlpha, iFail+iBeta))
+}
+
+
+## get the hyperparameters using all the data
+ivThetas.data = dfData.2$Hits/dfData.2$AtBats + 0.5
+## get hyperparameters using population data
+l = getalphabeta(mean(ivThetas.data), var(ivThetas.data))
+
+## use the data to calculate success and failures
+suc = dfData.2$Hits
+fail = dfData.2$AtBats - dfData.2$Hits
+# put data in matrix form to cycle the function over each row
+m = cbind(l['alpha'], l['beta'], suc, fail)
+
+mPositions.conj = apply(m, 1, getFittedTheta)
+
+## use these to estimate the parameters for each level 2
+## lets put this into a function to make it simpler
+getBetaBinomialPosterior = function(suc, trials, grouping, prior.alpha, prior.beta){
+  s = tapply(suc, grouping, sum)
+  t = tapply(trials, grouping, sum)
+  m = cbind(prior.alpha, prior.beta, s, t-s)
+  return(apply(m, 1, getTheta))
+}
+
+
+
+
+
+
+
+
+
+#####################################################################
+library(car) ## for logit function
+library(LearnBayes)
+library(numDeriv)
+logit.inv = function(p) {exp(p)/(exp(p)+1) }
+
+
+mylogpost = function(theta, data){
+  ## parameters to track/estimate
+  iTheta = logit.inv(theta[grep('theta', names(theta))])
+  iOmega1 = logit.inv(theta[grep('omega1', names(theta))])
+  iKappa1 = exp(theta[grep('kappa1', names(theta))])
+
+  ## data, binomial
+  iSuc = data$success # resp
+  iTotal = data$trials
+  groupIndex = data$groupIndex # mapping variable
+
+  # checks on parameters
+  if (any(iKappa1 < 2)) return(-Inf)
+  # write the priors and likelihood
+  lp1 = sum(dbeta(iOmega1, 0.5, 0.5, log=T)) + sum(dgamma(iKappa1, 0.5, 1e-4, log=T))
+  ## calculate the parameters for the next level - i.e. first grouping level
+  a = iOmega1*(iKappa1-2)+1
+  b = (1-iOmega1)*(iKappa1-2)+1
+  lp2 = sum(dbeta(iTheta, a[groupIndex], b[groupIndex], log=T))
+  lik = sum(dbinom(iSuc, iTotal, iTheta, log=T))
+  val = lik + lp1 + lp2
+  return(val)
+}
+
+s1 = dfData$Hits / dfData$AtBats
+names(s1) = rep('theta', times=length(s1))
+s1 = logit(s1)
+
+s2 = tapply(s1, dfData$PriPos, mean)
+names(s2) = rep('omega1', times=length(s2))
+
+s3 = rep(log(1000), times=nlevels(dfData$PriPos))
+names(s3) = rep('kappa1', times=length(s3))
+
+start = c(s1, s2, s3)
+lData = list('success' = dfData$Hits,
+             'trials' = dfData$AtBats,
+             'groupIndex' = as.numeric(dfData$PriPos))
+
+mylogpost(start, lData)
+laplace(mylogpost, start, lData)
+
+
+
+########### try a conjugate approach 
+## get alpha and beta parameters for the beta distribution from the data
+## see gelman p 583
+getalphabeta = function(m, v){
+  al.be = (m * (1-m) / v) - 1
+  al = al.be * m
+  be = al.be * (1-m)
+  return(c(alpha=al, beta=be))
+}
+
+getTheta = function(param){
+  iAlpha = param[1];
+  iBeta = param[2];
+  iSuc = param[3];
+  iFail = param[4];
+  return(rbeta(1000, iSuc+iAlpha, iFail+iBeta))
+}
+
+
+
+## get the hyperparameters from all the data
+ivThetas.data = dfData$Hits/dfData$AtBats + 0.5
+## get hyperparameters using population data
+l = getalphabeta(mean(ivThetas.data), var(ivThetas.data))
+
+## get the parameters for the first level 
+s = tapply(dfData$Hits, dfData$PriPos, sum)
+t = tapply(dfData$AtBats, dfData$PriPos, sum)
+m = cbind(l['alpha'], l['beta'], s, t-s)
+
+mLevel1 = apply(m, 1, getTheta)
+
+## use these to estimate the parameters for each level 2
+## lets put this into a function to make it simpler
+getBetaBinomialPosterior = function(suc, trials, grouping, prior.alpha, prior.beta){
+  s = tapply(suc, grouping, sum)
+  t = tapply(trials, grouping, sum)
+  m = cbind(prior.alpha, prior.beta, s, t-s)
+  return(apply(m, 1, getTheta))
+}
+
+mLevel2 = getBetaBinomialPosterior(dfData$Hits, dfData$AtBats, dfData$Player, l['alpha'], l['beta'])
+
+
+# calculate priors for each sub group
+mLevel2 = sapply(seq_along(levels(dfData$PriPos)), function(x) {
+  lvl = levels(dfData$PriPos)[x]
+  df = dfData[dfData$PriPos == lvl,]
+  df = droplevels.data.frame(df)
+  ivThetas.data = df$Hits/df$AtBats + 0.5
+  ## get hyperparameters using population data
+  l = getalphabeta(mean(ivThetas.data), var(ivThetas.data))
+  return(getBetaBinomialPosterior(df$Hits, df$AtBats, df$Player, l['alpha'], l['beta']))
+})
+
+mLevel2 = do.call(cbind, mLevel2)
+
+l = getalphabeta(mean(colMeans(mLevel1)), var(colMeans(mLevel1)))
+mLevel2 = getBetaBinomialPosterior(dfData$Hits, dfData$AtBats, dfData$Player, l['alpha'], l['beta'])
+
+
+
+
+## try a conjugate approach
+# getTheta = function(iAlpha, iBeta, iSuc, iFail){
+#   return(mean(rbeta(1000, iSuc+iAlpha, iFail+iBeta)))
+# }
+
+
+
+## get prior for each group
+s1 = dfData$methylated / dfData$total
+m = tapply(s1, dfData$interaction, mean)
+v = tapply(s1, dfData$interaction, var)
+m = cbind(m, v)
+l = sapply(1:nrow(m), function(x) getalphabeta(m[x,1], m[x,2]))
+alpha = l['alpha',]
+beta = l['beta',]
+alpha = alpha[as.numeric(dfData$interaction)]
+beta = beta[as.numeric(dfData$interaction)]
+suc = dfData$methylated
+fail = dfData$total - dfData$methylated
+m = cbind(alpha, beta, suc, fail)
+apply(m, 1, getTheta)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 ########### load second smaller data set with less parameters at lvl 2
 ## the data comes from a Bisulphite sequencing experiment 
 ## we look at the number of methylated cytosine bases at a certain position in the genome
