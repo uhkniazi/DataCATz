49ed8e7ce7ed2e081bcc7f51b6913018e9562c5e
Umar Niazi <uhkniazi@gmail.com>
2018-01-30 11:24:54 +0000

    in progress, testing with normal model then t model

--- BayesRegression/highDemensionRegression.R
+++ BayesRegression/highDemensionRegression.R
@@ -22,22 +22,66 @@ gammaShRaFromModeSD = function( mode , sd ) {
   return( list( shape=shape , rate=rate ) )
 }
 
+f_plotVolcano = function(dfGenes, main, p.adj.cut = 0.1, fc.lim = c(-3, 3)){
+  p.val = -1 * log10(dfGenes$P.Value)
+  fc = dfGenes$logFC
+  # cutoff for p.value y.axis
+  y.cut = -1 * log10(0.01)
+  col = rep('lightgrey', times=length(p.val))
+  c = which(dfGenes$adj.P.Val < p.adj.cut)
+  col[c] = 'red'
+  plot(fc, p.val, pch=20, xlab='Fold Change', ylab='-log10 P.Value', col=col, main=main, xlim=fc.lim)
+  abline(v = 0, col='grey', lty=2)
+  abline(h = y.cut, col='red', lty=2)
+  # second cutoff for adjusted p-values
+  y.cut = quantile(p.val[c], probs=0.95)
+  abline(h = y.cut, col='red')
+  # identify these genes
+  g = which(p.val > y.cut)
+  lab = dfGenes[g, 'SYMBOL']
+  text(dfGenes$logFC[g], y = p.val[g], labels = lab, pos=2, cex=0.6)
+}
 
+plotMeanFC = function(m, dat, p.cut, title){
+  col = rep('grey', length.out=(nrow(dat)))
+  col[dat$adj.P.Val < p.cut] = 'red'
+  #mh = cut(m, breaks=quantile(m, 0:50/50), include.lowest = T)
+  plot(m, dat$logFC, col=col, pch=20, main=title, xlab='log Mean', ylab='logFC', ylim=c(-2, 2), cex=0.6)
+}
 
 lData = f_LoadObject(file.choose())
 m = lData$data
-## remove missing data vectors
-dim(m)
-table(rowSums(m) <= 0)
-f = rowSums(m) <= 0
-m = m[!f,]
 
 dfData = data.frame(t(m))
 dfData = stack(dfData)
-dfData$fBatch = lData$batch
+dfData$fBatch = lData$cov1
+dfData$fAdjust = lData$cov2
 dfData$Coef = factor(dfData$fBatch:dfData$ind)
 dfData = dfData[order(dfData$Coef), ]
-#### fit mixed effect model
+
+
+###### fit a model using limma
+library(limma)
+
+#fBatch = lData$cov1; fAdjust=lData$cov2;
+design = model.matrix(~ lData$cov1)
+colnames(design) = levels(lData$cov1)
+head(design)
+
+fit = lmFit(lData$data, design)
+fit = eBayes(fit)
+
+dfLimmma.2 = topTable(fit, coef=2, adjust='BH', number = Inf)
+dfLimmma.0 = topTable(fit, coef=3, adjust='BH', number = Inf)
+
+
+
+
+
+
+
+
+# #### fit mixed effect model
 library(lme4)
 fit.lme1 = lmer(values ~ 1 + (1 | Coef), data=dfData, REML=F)
 summary(fit.lme1)
@@ -50,10 +94,16 @@ library(rstan)
 rstan_options(auto_write = TRUE)
 options(mc.cores = parallel::detectCores())
 
-stanDso = rstan::stan_model(file='tResponse2RandomEffectsNoFixed.stan')
+stanDso = rstan::stan_model(file='tResponse1RandomEffectsNoFixed.stan')
 
 ## calculate hyperparameters for variance of coefficients
 l = gammaShRaFromModeSD(sd(dfData$values), 2*sd(dfData$values))
+
+## set initial values
+initf = function(chain_id = 1) {
+  gm = tapply(dfData$values, dfData$Coef, mean) - mean(dfData$values)
+  list(betas = mean(dfData$values), sigmaRan1 = sd(gm), sigmaPop=sd(dfData$values), nu=4, rGroupsJitter1=gm)
+}
 
 ### try a t model without mixture
 lStanData = list(Ntotal=nrow(dfData), Nclusters1=nlevels(dfData$Coef),
@@ -64,63 +114,113 @@ lStanData = list(Ntotal=nrow(dfData), Nclusters1=nlevels(dfData$Coef),
                  y=dfData$values, 
                  gammaShape=l$shape, gammaRate=l$rate)
 
-fit.stan = sampling(stanDso, data=lStanData, iter=5000, chains=2, 
+fit.stan = sampling(stanDso, data=lStanData, iter=1000, chains=2, 
                     pars=c('betas', 'sigmaRan1', #'sigmaRan2',
-                           'nu', 'sigmaPop','mu', 
+                           'nu', 'sigmaPop', #'mu', 
                            'rGroupsJitter1'), #'rGroupsJitter2'),
-                    cores=2)#, control=list(adapt_delta=0.99, max_treedepth = 15))
+                    cores=2, init=initf)#, control=list(adapt_delta=0.99, max_treedepth = 15))
 print(fit.stan, c('betas', 'sigmaRan1', 'sigmaPop', 'nu'), digits=3)
 
-## model checks
-m = extract(fit.stan, 'mu')
-names(m)
-dim(m$mu)
-fitted = apply(m$mu, 2, mean)
 
-plot(dfData$values, fitted, pch=20, cex=0.5)
-plot(dfData$values, dfData$values - fitted, pch=20, cex=0.5)
-iResid = (dfData$values - fitted)
+## get the coefficient of interest - Modules in our case from the random coefficients section
+mModules = extract(fit.stan)$rGroupsJitter1
+dim(mModules)
+## get the intercept at population level
+iIntercept = as.numeric(extract(fit.stan)$betas)
+## add the intercept to each random effect variable, to get the full coefficient
+mModules = sweep(mModules, 1, iIntercept, '+')
 
-par(mfrow=c(1,2))
-plot(fitted, iResid, pch=20, cex=0.5, main='t model')
-lines(lowess(fitted, iResid), col=2, lwd=2)
-
-plot(predict(fit.lme1), resid(fit.lme1), pch=20, cex=0.5, main='normal')
-lines(lowess(predict(fit.lme1), resid(fit.lme1)), col=2, lwd=2)
-
-plot(fitted, predict(fit.lme1), pch=20, cex=0.5)
-
-### plot the posterior predictive values
-m = extract(fit.stan, c('mu', 'nu', 'sigmaPop'))
-i = sample(1:5000, 5000)
-muSample = m$mu[i,]
-nuSample = m$nu[i]
-sigSample = m$sigmaPop[i]
-
-## t sampling functions
-dt_ls = function(x, df, mu, a) 1/a * dt((x - mu)/a, df)
-rt_ls <- function(n, df, mu, a) rt(n,df)*a + mu
-
-## use point-wise predictive approach to sample a new value from this data
-ivResp = dfData$values
-mDraws = matrix(NA, nrow = length(ivResp), ncol=2000)
-
-# rppd = function(index){
-#   f = muSample[,index]
-#   return(rt_ls(length(f), nuSample, f, sigSample))
-# }
-
-for (i in 1:ncol(mDraws)){
-  mDraws[,i] = rt_ls(length(ivResp), nuSample[i], muSample[i,], sigSample[i])
+## function to calculate statistics for differences between coefficients
+getDifference = function(ivData, ivBaseline){
+  stopifnot(length(ivData) == length(ivBaseline))
+  # get the difference vector
+  d = ivData - ivBaseline
+  # get the z value
+  z = mean(d)/sd(d)
+  # get 2 sided p-value
+  p = pnorm(-abs(mean(d)/sd(d)))*2
+  return(list(z=z, p=p))
 }
 
-# 
-# temp = sapply(1:length(ivResp), function(x) rppd(x))
-# mDraws = t(temp)
+## split the data into the comparisons required
+d = data.frame(cols=1:ncol(mModules), mods=levels(dfData$Coef))
+## split this factor into sub factors
+f = strsplit(as.character(d$mods), ':')
+d = cbind(d, do.call(rbind, f))
+head(d)
+colnames(d) = c(colnames(d)[1:2], c('fBatch', 'ind'))
+d$split = factor(d$ind)
 
-yresp = density(ivResp)
-plot(yresp, xlab='', main='Fitted distribution', ylab='density', lwd=2)#, ylim=c(0, 1))
-temp = apply(mDraws, 2, function(x) {x = density(x)
-#x$y = x$y/max(x$y)
-lines(x, col='red', lwd=0.6)
+## get a p-value for each comparison
+l = tapply(d$cols, d$split, FUN = function(x, base='12', deflection='2') {
+  c = x
+  names(c) = as.character(d$fBatch[c])
+  dif = getDifference(ivData = mModules[,c[deflection]], ivBaseline = mModules[,c[base]])
+  r = data.frame(ind= as.character(d$ind[c[base]]), coef.base=mean(mModules[,c[base]]), 
+                 coef.deflection=mean(mModules[,c[deflection]]), zscore=dif$z, pvalue=dif$p)
+  r$difference = r$coef.deflection - r$coef.base
+  #return(format(r, digi=3))
+  return(r)
 })
+
+dfResults = do.call(rbind, l)
+dfResults$p.adj = format(p.adjust(dfResults$pvalue, method='BH'), digi=3)
+write.csv(dfResults, file='temp/stan.csv', row.names = F)
+
+
+
+
+
+# ## model checks
+# m = extract(fit.stan, 'mu')
+# names(m)
+# dim(m$mu)
+# fitted = apply(m$mu, 2, mean)
+# 
+# plot(dfData$values, fitted, pch=20, cex=0.5)
+# plot(dfData$values, dfData$values - fitted, pch=20, cex=0.5)
+# iResid = (dfData$values - fitted)
+# 
+# par(mfrow=c(1,2))
+# plot(fitted, iResid, pch=20, cex=0.5, main='t model')
+# lines(lowess(fitted, iResid), col=2, lwd=2)
+# 
+# plot(predict(fit.lme1), resid(fit.lme1), pch=20, cex=0.5, main='normal')
+# lines(lowess(predict(fit.lme1), resid(fit.lme1)), col=2, lwd=2)
+# 
+# plot(fitted, predict(fit.lme1), pch=20, cex=0.5)
+# 
+# ### plot the posterior predictive values
+# m = extract(fit.stan, c('mu', 'nu', 'sigmaPop'))
+# i = sample(1:5000, 5000)
+# muSample = m$mu[i,]
+# nuSample = m$nu[i]
+# sigSample = m$sigmaPop[i]
+# 
+# ## t sampling functions
+# dt_ls = function(x, df, mu, a) 1/a * dt((x - mu)/a, df)
+# rt_ls <- function(n, df, mu, a) rt(n,df)*a + mu
+# 
+# ## use point-wise predictive approach to sample a new value from this data
+# ivResp = dfData$values
+# mDraws = matrix(NA, nrow = length(ivResp), ncol=2000)
+# 
+# # rppd = function(index){
+# #   f = muSample[,index]
+# #   return(rt_ls(length(f), nuSample, f, sigSample))
+# # }
+# 
+# for (i in 1:ncol(mDraws)){
+#   mDraws[,i] = rt_ls(length(ivResp), nuSample[i], muSample[i,], sigSample[i])
+# }
+# 
+# # 
+# # temp = sapply(1:length(ivResp), function(x) rppd(x))
+# # mDraws = t(temp)
+# 
+# yresp = density(ivResp)
+# plot(yresp, xlab='', main='Fitted distribution', ylab='density', lwd=2)#, ylim=c(0, 1))
+# temp = apply(mDraws, 2, function(x) {x = density(x)
+# #x$y = x$y/max(x$y)
+# lines(x, col='red', lwd=0.6)
+# })
--- BayesRegression/tResponse1RandomEffectsNoFixed.stan
+++ BayesRegression/tResponse1RandomEffectsNoFixed.stan
@@ -1,10 +1,10 @@
 data {
   int<lower=1> Ntotal; // number of observations
   int<lower=1> Nclusters1; // number of levels for group 1 for random intercepts
-  int<lower=1> Nclusters2; // number of levels for group 2 for random intercepts
+  //int<lower=1> Nclusters2; // number of levels for group 2 for random intercepts
   int<lower=1, upper=Nclusters1> NgroupMap1[Ntotal]; // mapping variable to map each observation to group 1 
-  int<lower=1, upper=Nclusters2> NgroupMap2[Ntotal]; // mapping variable to map each observation to group 2 
-  int<lower=1> Ncol; // total number of columns in model matrix
+  //int<lower=1, upper=Nclusters2> NgroupMap2[Ntotal]; // mapping variable to map each observation to group 2 
+  //int<lower=1> Ncol; // total number of columns in model matrix
   real y[Ntotal]; // response variable normally distributed
   // additional parameters
   real gammaShape; // hyperparameters for the gamma distribution 
@@ -14,27 +14,27 @@ data {
   // }
 parameters {
   // parameters to estimate in the model
-  vector[Ncol] betas; // regression parameters
+  real betas; // regression intercept 
   real<lower=0> sigmaRan1; // random effect standard deviation for group 1
-  real<lower=0> sigmaRan2; // random effect standard deviation for group 2
+  //real<lower=0> sigmaRan2; // random effect standard deviation for group 2
   real<lower=0> sigmaPop; // population standard deviation
   vector[Nclusters1] rGroupsJitter1; // number of random jitters for each level of cluster/group 1
-  vector[Nclusters2] rGroupsJitter2; // number of random jitters for each level of cluster/group 2
+  //vector[Nclusters2] rGroupsJitter2; // number of random jitters for each level of cluster/group 2
   real<lower=1> nu; // normality parameter for t distribution or degree of freedom 
 }
 transformed parameters {
   vector[Ntotal] mu; // fitted values from linear predictor
-  mu = betas[1] + rGroupsJitter1[NgroupMap1] + rGroupsJitter2[NgroupMap2];
+  mu = betas + rGroupsJitter1[NgroupMap1]; // + rGroupsJitter2[NgroupMap2];
 }
 model {
   nu ~ exponential(1/29.0);
   sigmaRan1 ~ gamma(gammaShape, gammaRate);
-  sigmaRan2 ~ gamma(gammaShape, gammaRate);
+  //sigmaRan2 ~ gamma(gammaShape, gammaRate);
   sigmaPop ~ gamma(gammaShape, gammaRate);
   betas ~ cauchy(0, 10);
   // random effects sample
   rGroupsJitter1 ~ normal(0, sigmaRan1);
-  rGroupsJitter2 ~ normal(0, sigmaRan2);
+  //rGroupsJitter2 ~ normal(0, sigmaRan2);
   // likelihood function
   y ~ student_t(nu, mu, sigmaPop);
 }
