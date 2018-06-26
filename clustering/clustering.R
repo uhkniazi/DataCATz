ffad7f7fae8474d9e03025b1caeace494de86298
Umar Niazi <uhkniazi@gmail.com>
2018-06-26 15:46:18 +0100

    keeping the clustering section aside at the moment and doing the incumbent data set

--- clustering/clustering.R
+++ clustering/clustering.R
@@ -5,174 +5,108 @@
 
 library(iCluster)
 
-data("breast.chr17")
-?breast.chr17
+p.old = par()
 
-mData.rna = breast.chr17$mRNA.data
-mData.dna = breast.chr17$DNA.data
-
-################################ some basic exploratory analysis of the data
-library(downloader)
-url = 'https://raw.githubusercontent.com/uhkniazi/CDiagnosticPlots/master/CDiagnosticPlots.R'
-download(url, 'CDiagnosticPlots.R')
-
-# load the required packages
-source('CDiagnosticPlots.R')
-# delete the file after source
-unlink('CDiagnosticPlots.R')
-
-mData = t(mData.rna)
-dim(mData)
-colnames(mData)
-fBatch = gsub('(\\w+)\\..+', '\\1', colnames(mData))
-fBatch[!(fBatch %in% c('NORWAY', 'STANFORD'))] = 'Lines'
-fBatch = factor(fBatch)
-
-oDiag.1 = CDiagnosticPlots(mData, 'RNA')
-
-boxplot.median.summary(oDiag.1, fBatch, legend.pos = 'topright', axis.label.cex = 0.7)
-plot.mean.summary(oDiag.1, fBatch, axis.label.cex = 0.7)
-plot.sigma.summary(oDiag.1, fBatch, axis.label.cex = 0.7)
-plot.PCA(oDiag.1, fBatch, cex.main=1)
-plot.dendogram(oDiag.1, fBatch, labels_cex = 0.5, cex.main=0.7)
-
-## second data set
-mData = t(mData.dna)
-dim(mData)
-colnames(mData)
-fBatch = gsub('(\\w+)\\..+', '\\1', colnames(mData))
-fBatch[!(fBatch %in% c('NORWAY', 'STANFORD'))] = 'Lines'
-fBatch = factor(fBatch)
-
-oDiag.2 = CDiagnosticPlots(mData, 'DNA')
-
-boxplot.median.summary(oDiag.2, fBatch, legend.pos = 'topright', axis.label.cex = 0.7)
-plot.mean.summary(oDiag.2, fBatch, axis.label.cex = 0.7)
-plot.sigma.summary(oDiag.2, fBatch, axis.label.cex = 0.7)
-plot.PCA(oDiag.2, fBatch, cex.main=1)
-plot.dendogram(oDiag.2, fBatch, labels_cex = 0.5, cex.main=0.7)
-
-### cluster both data sets together using iCluster
-fit=iCluster(breast.chr17, k=4, lambda=c(0.2,0.2))
-plotiCluster(fit=fit, label=rownames(breast.chr17[[2]]))
-
-######## try clustering the data using PCA model
-
-## whiten the data
-whiten = function(mData){
-  ## center the data
-  ivMeans = colMeans(mData)
-  # centered data
-  mData.s = sweep(mData, 2, ivMeans, '-')
-  ## calculate covariance matrix
-  mCov = cov(mData)
-  ## see bishop 2006 chapter 12 page 568 for formula
-  # y = 1/sqrt(L) * t(U) * centered data
-  ## get the eigen vectors and values
-  lEigens = eigen(mCov)
-  L = diag(lEigens$values)
-  U = lEigens$vectors
-  # invert after taking square root
-  Z = solve(sqrt(L))
-  Z = Z %*% t(U)
-  yn = Z %*% t(mData.s)
-  rownames(yn) = colnames(mData)
-  return(t(yn))
+# utility function to load object
+f_LoadObject = function(r.obj.file)
+{
+  # temp environment to load object
+  env <- new.env()
+  # read object
+  nm <- load(r.obj.file, env)[1]
+  return(env[[nm]])
 }
 
-mData.white = whiten(t(mData.dna))
+lData.train = f_LoadObject('~/Data/R/CGraphClust/workflow/results/lData.train.rds')
+names(lData.train)
 
-oDiag.3 = CDiagnosticPlots(mData.white, 'white dna')
+oExp = lData.train$data
 
-boxplot.median.summary(oDiag.3, fBatch, legend.pos = 'topright', axis.label.cex = 0.7)
-plot.mean.summary(oDiag.3, fBatch, axis.label.cex = 0.7)
-plot.sigma.summary(oDiag.3, fBatch, axis.label.cex = 0.7)
-plot.PCA(oDiag.3, fBatch, cex.main=1)
-plot.dendogram(oDiag.3, fBatch, labels_cex = 0.5, cex.main=0.7)
+## take a sample
+set.seed(123)
+i = sample(1:537, 60)
+oExp = oExp[,i]
+dim(oExp)
 
-######################################################################################
-## extract components 
-pr.dna = prcomp(mData.dna, scale=T, center = T)
-pr.rna = prcomp(mData.rna, scale=T, center = T)
+mCounts = t(exprs(oExp))
+dim(mCounts)
+str(mCounts)
+fGroups = oExp$fSamples
+table(fGroups)
+rownames(mCounts) = as.character(fGroups)
 
-plot(pr.dna$sdev)
-plot(pr.rna$sdev)
+#### randomly split the data set into 2 parts
+## sub select top genes sorted on p-values
+cvTopGenes = rownames(lData.train$results)[1:50]
+mCounts = mCounts[,cvTopGenes]
+dim(mCounts)
+set.seed(123)
+i = sample(1:ncol(mCounts), 25)
+mCounts.1 = mCounts[,i]
+mCounts.2 = mCounts[,-i]
+str(mCounts.1)
+lData = list(mCounts.1, mCounts.2)
+### cluster both data sets together using iCluster
+fit=iCluster(lData, k=3, lambda=c(0.2,0.2))
+plotiCluster(fit=fit, label=rownames(mCounts))
 
-## take the 6 components from both data sets
-c1 = pr.dna$x#[,1:6]
-c2 = pr.rna$x#[,1:6]
-
-mData = cbind(c1, c2)
-mData = scale(mData)
-pr.comb = prcomp(mData)
-plot(pr.comb$x, col=ivCols, pch=20, main='Analytical')
-text(pr.comb$x, labels = rownames(mData), adj = 1, cex = 0.6)
-plot(pr.comb$sdev)
+######## try clustering the data using PCA model
+ivCols = rainbow(n = nlevels(fGroups))
+ivCols = ivCols[as.numeric(fGroups)]
+pr.comp = prcomp(mCounts)
+plot(pr.comp$x, col=ivCols, pch=20, main='Analytical')
+text(pr.comp$x, labels = rownames(mCounts), adj = 1, cex = 0.6)
+legend('bottomright', legend = levels(fGroups), fill = rainbow(n = nlevels(fGroups)))
+plot(pr.comp$sdev)
 ######### lets try a bayesian approach
 ### first get the estimates using stan and mcmc
+mData = mCounts
 library(rstan)
 rstan_options(auto_write = TRUE)
 options(mc.cores = parallel::detectCores())
 
-#mData = mData.dna
-mData = cbind(mData.dna, mData.rna)
-## compile the first stan script
-stanDso = rstan::stan_model(file='pcaAndVectors/bayesianPCA.stan')
-
-## set stan data
-lStanData = list(Ntotal=nrow(mData), Nvars=ncol(mData), Neigens=2,
-                 y=(mData))
-
-# ## calculate initial values
-# initf = function(chain_id = 1) {
-#   list(mEigens=pr.out$rotation)
-# }
-
-fit.stan = sampling(stanDso, data=lStanData, iter=5000, chains=3, cores=3)#, init=initf)
-#control=list(adapt_delta=0.99, max_treedepth = 10)) # some additional control values/not necessary usually
-save(fit.stan, file='Temp/fit.stan.rds')
-print(fit.stan)
-
-## extract the results
-lResults = extract(fit.stan)
-## extract the latent variables
-mComp = lResults$mComponents
-dim(mComp)
-## get the first 2 latent variables for plotting
-mComp.1 = mComp[,,1]
-ivComp.1 = colMeans(mComp.1)
-mComp.2 = mComp[,,2]
-ivComp.2 = colMeans(mComp.2)
-
-ivCols = rainbow(n = nlevels(fBatch))
-ivCols = ivCols[as.numeric(fBatch)]
-par(mfrow=c(1,2))
-plot(ivComp.2, ivComp.1, pch=20, col=ivCols, xlab='PC2', ylab='PC1', main='MCMC 1')
-text(ivComp.2, ivComp.1, labels = rownames(mData), adj = 1, cex = 0.6)
-pr.out = prcomp(mData, scale=F, center = T)
-plot(pr.out$x, col=ivCols, pch=20, main='Analytical')
-text(pr.out$x, labels = rownames(mData), adj = 1, cex = 0.6)
-
-
 ## try a second approach with an additional parameter for variance of eigen vectors
-stanDso2 = rstan::stan_model(file='pcaAndVectors/bayesianPCA2.stan')
+# stanDso2 = rstan::stan_model(file='pcaAndVectors/bayesianPCA2.stan')
+# 
+# dim(mData)
+# ## set stan data
+# lStanData = list(Ntotal=nrow(mData), Nvars=ncol(mData), Neigens=7,
+#                  y=(mData))
 
-## set stan data
-lStanData = list(Ntotal=nrow(mData), Nvars=ncol(mData), Neigens=41,
-                 y=(mData))
+# utility function to calculate gamma distribution used as a prior for scale
+gammaShRaFromModeSD = function( mode , sd ) {
+  # function changed a little to return jeffery non-informative prior
+  if ( mode <=0 || sd <=0 ) return( list( shape=0.5 , rate=0.0001 ) ) 
+  rate = ( mode + sqrt( mode^2 + 4 * sd^2 ) ) / ( 2 * sd^2 )
+  shape = 1 + mode * rate
+  return( list( shape=shape , rate=rate ) )
+}
 
-pr.out = prcomp(mData, scale=F, center = F)
+stanDso2 = rstan::stan_model(file='clustering/bayesianPCAwithBatch.stan')
+
+l = gammaShRaFromModeSD(sd(mData), 2*sd(mData))
+
+fGroups = oExp$fHIV
+
+lStanData = list(Ntotal=nrow(mData), Nvars=ncol(mData), Neigens=3,
+                 Nclusters1=nlevels(fGroups),
+                 NgroupMap1=as.numeric(fGroups),
+                 y=(mData),
+                 gammaShape=l$shape, gammaRate=l$rate)
+
+pr.out = prcomp(mData, scale=F, center = T)
 plot(pr.out$x, col=ivCols, pch=20, main='Analytical')
 text(pr.out$x, labels = rownames(mData), adj = 1, cex = 0.6)
 plot(pr.out$sdev)
 
-fit.stan2 = sampling(stanDso2, data=lStanData, iter=20000, chains=3, cores=3)#, #init=initf, 
+fit.stan2 = sampling(stanDso2, data=lStanData, iter=5000, chains=3, cores=3)#, #init=initf, 
 #control=list(adapt_delta=0.99, max_treedepth = 10)) # some additional control values/not necessary usually
-save(fit.stan2, file='Temp/fit.stan2.rds')
+save(fit.stan2, file='Temp/fit.stan2.tb.batch.rds')
 print(fit.stan2)
 
 ## extract the results
 lResults = extract(fit.stan2)
+names(lResults)
 ## extract the variances
 mSigma2 = lResults$sigma2
 summary(mSigma2)
@@ -182,8 +116,8 @@ iSigma2 = colMeans(mSigma2)
 iOrder = order(iSigma2, decreasing = T)
 par(mfrow=c(1,2))
 plot(iSigma2[iOrder], type='b', xlab='Index', ylab='Standard Deviation', main='MCMC SD Eigen Vec')
-pr.out = prcomp(mData, scale=F, center = T)
-plot(pr.out$sdev, type='b', xlab='Index', ylab='Standard Deviation', main='Analytical SD')
+pr.out = prcomp(mData, scale=T, center = T)
+plot(pr.out$sdev[1:3], type='b', xlab='Index', ylab='Standard Deviation', main='Analytical SD')
 
 ## extract the latent variables
 mComp = lResults$mComponents
@@ -199,7 +133,6 @@ ivComp.2 = colMeans(mComp.2)
 par(mfrow=c(1,2))
 plot(1*ivComp.2, 1*ivComp.1, pch=20, col=ivCols, xlab='PC1', ylab='PC2', main='MCMC 2')
 text(1*ivComp.2, 1*ivComp.1, labels = rownames(mData), adj =c(0.5, -0.5) , cex = 0.6)
-pr.out = prcomp(mData, scale=F, center = F)
 plot(pr.out$x, col=ivCols, pch=20, main='Analytical')
 text(pr.out$x, labels = rownames(mData), adj = 1, cex = 0.6)
 
@@ -228,12 +161,12 @@ axis(1, 1:ncol(d2), colnames(d2), cex.axis=0.5, las=3)
 axis(2, 1:ncol(d2), rownames(d2), cex.axis=0.4, las=1)
 
 dim(mComp)
-mComp.all = lapply(seq_along(1:ncol(mComp)), function(x){
+mComp.all = lapply(seq_along(1:dim(mComp)[3]), function(x){
   colMeans(mComp[,,iOrder[x]])
 })
 
 mComp.all = do.call(cbind, mComp.all)
-
+dim(mComp.all)
 m = mComp.all
 rownames(m) = rownames(mData)
 d = dist(m)
@@ -241,15 +174,21 @@ plot(hclust(d))
 d2 = as.matrix(d)
 dim(d2)
 d2 = d2[nrow(d2):1,]
-image(1:ncol(d2), 1:ncol(d2), d2, axes=F, xlab='', ylab='', col=heat.colors(5))
-axis(1, 1:ncol(d2), colnames(d2), cex.axis=0.5, las=3)
-axis(2, 1:ncol(d2), rownames(d2), cex.axis=0.4, las=1)
+image(1:ncol(d2), 1:ncol(d2), d2, axes=F, xlab='', ylab='', col=heat.colors(3))
+axis(1, 1:ncol(d2), colnames(d2), cex.axis=0.2, las=3)
+axis(2, 1:ncol(d2), rownames(d2), cex.axis=0.2, las=1)
 
-k = kmeans(m, 4)
+k = kmeans(m, 3)
 k$cluster
+
+fGroups = oExp$fSamples
+table(k$cluster, fGroups)
+table(k$cluster, oExp$fHIV)
+table(k$cluster, oExp$fHIV:fGroups)
 c = sort(k$cluster)
 c
 d2 = d2[names(c), names(c)]
+dim(d2)
 image(1:ncol(d2), 1:ncol(d2), d2, axes=F, xlab='', ylab='', col=heat.colors(6))
 axis(1, 1:ncol(d2), colnames(d2), cex.axis=0.5, las=3)
 axis(2, 1:ncol(d2), rownames(d2), cex.axis=0.4, las=1)
