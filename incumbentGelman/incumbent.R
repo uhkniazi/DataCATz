0083d7da8afd8998933f2a4db22aef98a1608591
Umar Niazi <uhkniazi@gmail.com>
2018-07-03 16:34:18 +0100

    writing test quantities

--- incumbentGelman/incumbent.R
+++ incumbentGelman/incumbent.R
@@ -114,4 +114,196 @@ summary(cur)
 plot(prev, cur, pch=c(20,2)[as.numeric(dfData$treatment)], xlab='1986', ylab='1988',
      xlim=c(0,1), ylim=c(0,1), main='Democratic Vote in 1988 vs 1986')
 
+#####################################################################################################
+############# Data sets produced, now fit model
+#####################################################################################################
+fit.1 = lm(response ~ treatment + previousProportion + incumbentParty, data=dfData)
+summary(fit.1)
+
+## we can try the models in 2 ways, using a log posterior function in R and later in Stan
+library(LearnBayes)
+
+library(numDeriv)
+#library(optimx) optional optimiser to use
+# op = optimx(start, mylogpost, control = list(maximize=T, usenumDeriv=T, all.methods=T), data=lData)
+
+mylaplace = function (logpost, mode, data) 
+{
+  options(warn = -1)
+  fit = optim(mode, logpost, gr = NULL,  
+              control = list(fnscale = -1, maxit=10000), method='Nelder-Mead', data=data)
+  # calculate hessian
+  fit$hessian = (hessian(logpost, fit$par, data=data))
+  colnames(fit$hessian) = names(mode)
+  rownames(fit$hessian) = names(mode)
+  options(warn = 0)
+  mode = fit$par
+  h = -solve(fit$hessian)
+  stuff = list(mode = mode, var = h, converge = fit$convergence == 
+                 0)
+  return(stuff)
+}
+## write the log posterior function
+
+mylogpost = function(theta, data){
+  ## parameters to track/estimate
+  betas = theta[grep('betas', names(theta))] # vector of betas i.e. regression coefficients for population
+  sigmaPop = exp(theta['sigmaPop']) # population level sd
+  ## data
+  resp = data$resp # response variable
+  mModMatrix = data$mModMatrix # design matrix 
+  
+  # calculate fitted value
+  iFitted = mModMatrix %*% betas
+  # write the priors and likelihood 
+  lp = dcauchy(betas[1], 0, 5, log=T) + sum(dcauchy(betas[-1], 0, 2, log=T))
+  lik = sum(dnorm(resp,mean = iFitted, sd=sigmaPop, log=T))
+  val = lik + lp
+  return(val)
+}
+
+lData = list(resp=dfData$response, mModMatrix=model.matrix(response ~ treatment + previousProportion + incumbentParty, data=dfData))
+start = c('sigmaPop'=log(2), 'betas'=rep(0, times=ncol(lData$mModMatrix)))
+
+mylogpost(start, lData)
+
+fit.2 = mylaplace(mylogpost, start, lData)
+fit.2
+# compare with lm 
+data.frame(coef(fit.1), fit.2$mode[-1])
+se = sqrt(diag(fit.2$var))
+
+
+### lets take a sample from this using SIR
+## parameters for the multivariate t density
+tpar = list(m=fit.2$mode, var=fit.2$var*2, df=4)
+## get a sample directly and using sir (sampling importance resampling with a t proposal density)
+s = sir(mylogpost, tpar, 5000, lData)
+colnames(s)[-1] = colnames(lData$mModMatrix)
+apply(s, 2, mean)
+apply(s, 2, sd)
+exp(mean(s[,'sigmaPop']))
+pairs(s, pch=20)
+fit.2$sir = s
+
+#########################################################
+########## some model checks
+#########################################################
+
+# get the fitted value
+iBetas = colMeans(fit.2$sir)[-1]
+fitted = lData$mModMatrix %*% iBetas
+
+plot(dfData$response, fitted, pch=c(2,20)[as.numeric(dfData$treatment)], cex=0.5)
+# get residuals that is response minus fitted values
+iResid = (dfData$response - fitted)
+plot(dfData$response, iResid, pch=c(2,20)[as.numeric(dfData$treatment)], cex=0.5)
+
+plot(fitted, iResid, pch=c(2,20)[as.numeric(dfData$treatment)], cex=0.5, main='SIR')
+lines(lowess(fitted, iResid), col=2, lwd=2)
+
+plot(predict(fit.1), resid(fit.1), pch=c(2,20)[as.numeric(dfData$treatment)], cex=0.5, main='lm')
+lines(lowess(predict(fit.1), resid(fit.1)), col=2, lwd=2)
+
+## calculate standardized residuals
+library(MASS)
+plot(fitted, stdres(fit.1), pch=c(2,20)[as.numeric(dfData$treatment)], cex=0.5, main='standardized residuals - lm')
+lines(lowess(fitted, stdres(fit.1)), col=2, lwd=2)
+## see equation 14.7 in Gelman 2013
+s = exp(mean(fit.2$sir[,'sigmaPop']))
+plot(fitted, iResid/s, pch=c(2,20)[as.numeric(dfData$treatment)], cex=0.5, main='standardized residuals - SIR')
+lines(lowess(fitted, iResid/s), col=2, lwd=2)
+
+### generate some posterior predictive data
+## follow the algorithm in section 14.3 page 363 in Gelman 2013
+simulateOne = function(betas, sigma, mModMatrix){
+  f = mModMatrix %*% betas
+  yrep = rnorm(length(f), f, sigma)
+  return(yrep)
+}
+
+runRegression = function(yrep, df){
+  df$response = yrep
+  return(lm(response ~ treatment + previousProportion + incumbentParty, data=df))
+}
+
+getResiduals = function(fit.object){
+  return(resid(fit.object))
+}
+
+
+## sample n values, 1000 times
+mDraws.sim = matrix(NA, nrow = nrow(dfData), ncol=1000)
+mDraws.fitted = matrix(NA, nrow = nrow(dfData), ncol=1000)
+mDraws.res = matrix(NA, nrow = nrow(dfData), ncol=1000)
+
+for (i in 1:1000){
+  p = sample(1:nrow(fit.2$sir), 1)
+  sigma = exp(fit.2$sir[p,'sigmaPop'])
+  betas = fit.2$sir[p,-1]
+  mDraws.sim[,i] = simulateOne(betas, sigma, lData$mModMatrix)
+  mDraws.fitted[,i] = fitted(runRegression(mDraws.sim[,i], dfData))
+  mDraws.res[,i] = getResiduals(runRegression(mDraws.sim[,i], dfData))
+}
+
+### visual checks
+ivResp = dfData$response
+yresp = density(ivResp)
+plot(yresp, xlab='', main='Fitted distribution', ylab='density', lwd=2)#, ylim=c(0, 1))
+temp = apply(mDraws.sim, 2, function(x) {x = density(x)
+#x$y = x$y/max(x$y)
+lines(x, col='red', lwd=0.6)
+})
+lines(yresp)
+
+### plot the residuals
+plot(dfData$response, iResid, pch=c(2,20)[as.numeric(dfData$treatment)], cex=0.5, main='SIR')
+lines(lowess(dfData$response, iResid))
+temp = sapply(1:1000, function(x) lines(lowess(dfData$response, mDraws.res[,x]), lwd=0.5, col=2))
+
+###############################################################
+######### test quantities for model fits
+###############################################################
+## calculate bayesian p-value for this test statistic
+getPValue = function(Trep, Tobs){
+  left = sum(Trep <= Tobs)/length(Trep)
+  right = sum(Trep >= Tobs)/length(Trep)
+  return(min(left, right))
+}
+
+## test for variance
+T1_var = function(Y) return(var(Y))
+
+## min quantity
+T1_min = function(Y){
+  return(min(Y))
+} 
+
+## max quantity
+T1_max = function(Y){
+  return(max(Y))
+} 
+
+## mean quantity
+T1_mean = function(Y){
+  return(mean(Y))
+} 
+
+
+
+### search for outliers
+s = exp(mean(fit.2$sir[,'sigmaPop']))
+## 3 standard deviations 
+iResLimit = round(s*3, 1)
+
+## generate the test statistic
+## proportion of absolute values residuals that exceed limit iResLimit
+iProp.observed = sum(abs(iResid) > iResLimit)/length(iResid)
+
+T1_proportion = function(Y) {
+  return(sum(abs(Y) > iResLimit)/length(Y))
+}
+
+t1_proportion = apply(mDraws.res, 2, T1_proportion)
+t1_var = apply(mDraws.sim, 2, T1_var)
 
