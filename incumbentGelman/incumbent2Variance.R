0fa3ffa68e23043622bda92232a418c4dc933ed1
Umar Niazi <uhkniazi@gmail.com>
2018-07-12 16:31:54 +0100

    nearly done, produce figures for blog

--- incumbentGelman/incumbent2Variance.R
+++ incumbentGelman/incumbent2Variance.R
@@ -24,7 +24,7 @@ m = model.matrix(response ~ treatment + previousProportion + incumbentParty, dat
 lStanData = list(Ntotal=nrow(dfData), NgroupMap=as.numeric(dfData$treatment), Ncol=ncol(m), X=m,
                  y=dfData$response)
 
-fit.stan = sampling(stanDso, data=lStanData, iter=1000, chains=2, pars=c('betas', 'mu', 'sigmaPop'),
+fit.stan = sampling(stanDso, data=lStanData, iter=5000, chains=2, pars=c('betas', 'mu', 'sigmaPop'),
                     cores=2)
 print(fit.stan, c('betas', 'sigmaPop'), digits=3)
 
@@ -62,7 +62,7 @@ mTable[3, ] = round(quantile(s[,4], probs = c(0.025, 0.25, 0.5, 0.75, 0.975)), 3
 mTable[4, ] = round(quantile(s[,1], probs = c(0.025, 0.25, 0.5, 0.75, 0.975)), 3)
 mTable[5, ] = round(quantile((s[,5]), probs = c(0.025, 0.25, 0.5, 0.75, 0.975)), 3)
 mTable[6, ] = round(quantile((s[,6]), probs = c(0.025, 0.25, 0.5, 0.75, 0.975)), 3)
-
+mTable
 #########################################################
 ########## some model checks on 1988
 #########################################################
@@ -82,10 +82,12 @@ lines(lowess(fitted, iResid), col=2, lwd=2)
 plot(predict(fit.1), resid(fit.1), pch=c(2,20)[as.numeric(dfData$treatment)], cex=0.5, main='lm')
 lines(lowess(predict(fit.1), resid(fit.1)), col=2, lwd=2)
 
+plot(predict(fit.1), fitted, pch=c(2,20)[as.numeric(dfData$treatment)], cex=0.5)
 ## calculate standardized residuals
+## these are useful to detect non-normality
 library(MASS)
-plot(fitted, stdres(fit.1), pch=c(2,20)[as.numeric(dfData$treatment)], cex=0.5, main='standardized residuals - lm')
-lines(lowess(fitted, stdres(fit.1)), col=2, lwd=2)
+plot(predict(fit.1), stdres(fit.1), pch=c(2,20)[as.numeric(dfData$treatment)], cex=0.5, main='standardized residuals - lm')
+lines(lowess(predict(fit.1), stdres(fit.1)), col=2, lwd=2)
 ## see equation 14.7 in Gelman 2013
 s = colMeans(mStan[,5:6])
 s = s[as.numeric(dfData$treatment)]
@@ -109,11 +111,11 @@ runRegression = function(yrep, lStanData){
   return(f.s)
 }
 
-# returns standardized residuals
-getResiduals = function(fit.object, mModMatrix, groupMap, yrep){
+# returns residuals
+getResiduals = function(fit.object, mModMatrix, yrep){
   b = colMeans(extract(fit.object)$betas)
   f = mModMatrix %*% b
-  r = (yrep - f)/extract(fit.object)$sigmaPop[groupMap]
+  r = (yrep - f)
   return(r)
 }
 
@@ -122,7 +124,7 @@ getResiduals = function(fit.object, mModMatrix, groupMap, yrep){
 mDraws.sim = matrix(NA, nrow = nrow(dfData), ncol=1000)
 mDraws.fitted = matrix(NA, nrow = nrow(dfData), ncol=1000)
 mDraws.res = matrix(NA, nrow = nrow(dfData), ncol=1000)
-
+dim(mStan)
 for (i in 1:1000){
   p = sample(1:nrow(mStan), 1)
   sigma = mStan[p,5:6]
@@ -130,11 +132,9 @@ for (i in 1:1000){
   mDraws.sim[,i] = simulateOne(betas, sigma, lStanData$X, as.numeric(dfData$treatment))
   f.s = runRegression(mDraws.sim[,i], lStanData)
   mDraws.fitted[,i] = apply(extract(f.s)$mu, 2, mean)
-  mDraws.res[,i] = getResiduals(f.s, lStanData$X, groupMap = as.numeric(dfData$treatment),
+  mDraws.res[,i] = getResiduals(f.s, lStanData$X,
                                 mDraws.sim[,i])
 }
-
-## add a section here to get residuals not standardized
 
 ### visual checks
 ivResp = dfData$response
@@ -147,9 +147,8 @@ lines(x, col='red', lwd=0.6)
 lines(yresp)
 
 ### plot the residuals
-plot(dfData$response, iResid, pch=c(2,20)[as.numeric(dfData$treatment)], cex=0.5, main='SIR')
+plot(dfData$response, iResid, pch=c(2,20)[as.numeric(dfData$treatment)], cex=0.5, main='MCMC')
 lines(lowess(dfData$response, iResid))
-temp = sapply(1:1000, function(x) lines(lowess(dfData$response, mDraws.res[,x]), lwd=0.5, col=2))
 
 plot(density(iResid))
 g = apply(mDraws.res, 2, function(x) lines(density(x), lwd=0.5, col=2))
@@ -184,36 +183,30 @@ T1_mean = function(Y){
 } 
 
 ## proportion of absolute value residuals that exceed limit iResLimit
-T1_proportion = function(Y) {
-  return(sum(abs(Y) > iResLimit)/length(Y))
-}
-
 T1_proportion.open = function(Y) {
-  return(sum(abs(Y)[dfData$treatment == 'open'] > iResLimit)/length(Y[dfData$treatment == 'open']))
+  return(sum(abs(Y)[dfData$treatment == 'open'] > iResLimit['sigmaOpen'])/length(Y[dfData$treatment == 'open']))
 }
 
 T1_proportion.inc = function(Y) {
-  return(sum(abs(Y)[dfData$treatment == 'incumbent'] > iResLimit)/length(Y[dfData$treatment == 'incumbent']))
+  return(sum(abs(Y)[dfData$treatment == 'incumbent'] > iResLimit['sigmaIncumbent'])/length(Y[dfData$treatment == 'incumbent']))
 }
 
 
 ### search for outliers by checking the simulated residuals
-#s = exp(mean(fit.2$sir[,'sigmaPop']))
+s = colMeans(mStan)[5:6]
 ## 3 standard deviations - this is the sd for the normal distribution used in the likelihood
-iResLimit = 2; #round(s*3, 1)
+iResLimit = round(s*3, 1)
 
 ## generate the test statistics and the observed value of that statistic and p-values
 # observed numbers of outliers in residuals
-iProp.observed = sum(abs(iResid) > iResLimit)/length(iResid)
-iProp.observed.open = sum(abs(iResid)[dfData$treatment == 'open'] > iResLimit)/length(iResid[dfData$treatment == 'open'])
-iProp.observed.inc = sum(abs(iResid)[dfData$treatment == 'incumbent'] > iResLimit)/length(iResid[dfData$treatment == 'incumbent'])
+iProp.observed.open = sum(abs(iResid)[dfData$treatment == 'open'] > iResLimit['sigmaOpen'])/length(iResid[dfData$treatment == 'open'])
+iProp.observed.inc = sum(abs(iResid)[dfData$treatment == 'incumbent'] > iResLimit['sigmaIncumbent'])/length(iResid[dfData$treatment == 'incumbent'])
 iVar.observed = var(dfData$response)
 iMin.observed = min(dfData$response)
 iMax.observed = max(dfData$response)
 iMean.observed = mean(dfData$response)
 
 # simulated quantites using posterior predictive data
-iProp.sim = apply(mDraws.res, 2, T1_proportion)
 iProp.sim.open = apply(mDraws.res, 2, T1_proportion.open)
 iProp.sim.inc = apply(mDraws.res, 2, T1_proportion.inc)
 iVar.sim = apply(mDraws.sim, 2, T1_var)
@@ -221,12 +214,11 @@ iMin.sim = apply(mDraws.sim, 2, T1_min)
 iMax.sim = apply(mDraws.sim, 2, T1_max)
 iMean.sim = apply(mDraws.sim, 2, T1_mean)
 
-mChecks = matrix(NA, nrow=7, ncol=1)
-rownames(mChecks) = c('Variance', 'Residuals', 'Residual.open', 'Residual.inc', 'Max', 'Min', 'Mean')
+mChecks = matrix(NA, nrow=6, ncol=1)
+rownames(mChecks) = c('Variance', 'Residual.open', 'Residual.inc', 'Max', 'Min', 'Mean')
 colnames(mChecks) = c('Model')
 
 mChecks['Variance',] = getPValue(iVar.sim, iVar.observed)
-mChecks['Residuals', ] = getPValue(iProp.sim, iProp.observed)
 mChecks['Max',] = getPValue(iMax.sim, iMax.observed)
 mChecks['Min',] = getPValue(iMin.sim, iMin.observed)
 mChecks['Mean',] = getPValue(iMean.sim, iMean.observed)
@@ -246,44 +238,35 @@ for (yr in seq_along(ivYears)){
   dfData = dfMerged[dfMerged$year == ivYears[yr], ]
   
   ## fit models for each year
-  lData = list(resp=dfData$response, mModMatrix=model.matrix(response ~ treatment + previousProportion + incumbentParty, data=dfData))
-  start = c('sigmaPop'=log(2), 'betas'=rep(0, times=ncol(lData$mModMatrix)))
+  m = model.matrix(response ~ treatment + previousProportion + incumbentParty, data=dfData)
+  lStanData = list(Ntotal=nrow(dfData), NgroupMap=as.numeric(dfData$treatment), Ncol=ncol(m), X=m,
+                   y=dfData$response)
 
-  fit.2 = mylaplace(mylogpost, start, lData)
-  ### lets take a sample from this using SIR
-  ## parameters for the multivariate t density
-  tpar = list(m=fit.2$mode, var=fit.2$var*2, df=4)
-  ## get a sample directly and using sir (sampling importance resampling with a t proposal density)
-  s = sir(mylogpost, tpar, 1000, lData)
-  colnames(s)[-1] = colnames(lData$mModMatrix)
+  fit.stan = sampling(stanDso, data=lStanData, iter=5000, chains=2, pars=c('betas', 'mu', 'sigmaPop'),
+                      cores=2)
+  s = cbind(extract(fit.stan)$betas, extract(fit.stan)$sigmaPop)
+  colnames(s) = c(colnames(lStanData$X), 'sigmaOpen', 'sigmaIncumbent')
   lYears[[yr]] = s
 }
 
 names(lYears) = ivYears
 
-## utility function for plotting
-getms = function(f){
-  m = mean(f)
-  se = sd(f)
-  m.up = m+1.96*se
-  m.down = m-1.96*se
-  ret= c(m, m.up, m.down)
-  names(ret) = c('m', 'm.up', 'm.down')
-  return(ret)
-}
+colnames(lYears[[1]])
 
-mTreatment = sapply(lYears, function(x) x[,3])
+mIncumbent = sapply(lYears, function(x) x[,'sigmaIncumbent'])
+mOpen = sapply(lYears, function(x) x[,'sigmaOpen'])
 
-df = apply(mTreatment, 2, getms)
-x = 1:ncol(mTreatment)
+## drop the 1992 as there are some problems there with the data
+mIncumbent = mIncumbent[,-23]
+mOpen = mOpen[,-23]
 
-## reproduce figure 14.2 from gelman 2013 
-plot(x, df['m',], ylim=c(min(df), max(df)), pch=20, xlab='Year', 
-     ylab='estimated incumbency advantage', main='', xaxt='n')
-axis(1, at = x, labels = colnames(mTreatment), las=2)
-for(l in 1:ncol(df)){
-  lines(x=c(x[l], x[l]), y=df[c(2,3),l], lwd=0.5)
-}
+i = colMeans(mIncumbent)
+o = colMeans(mOpen)
+
+matplot(cbind(i, o), type='l', xaxt='n', lty=1:2, xlim=c(1, 22))
+axis(side = 1, at = 1:length(i), labels=colnames(mIncumbent), las=2)
+legend('topleft', legend = c('incumbent', 'open'), lty=1:2, col=1:2)
+
 
 
 
