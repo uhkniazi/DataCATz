02fff737821378bcd8c9393b13a53c7e0c53eefc
Umar Niazi <uhkniazi@gmail.com>
2021-09-09 15:06:31 +0100

    ATT, ATC and ATE calculated using analytical & regression estimates. Use weighting and matching to estimate ATC, ATT and ATE with more details

--- causalDags/matchingDemo1Ch5MorganWinship.R
+++ causalDags/matchingDemo1Ch5MorganWinship.R
@@ -1,64 +1,116 @@
-# Name: weightedRegressionMorganWinship.R
+# Name: matchingDemo1Ch5MorganWinship.R
 # Auth: uhkniazi
-# Date: 25/08/2021
-# Desc: calculate the ATE, ATC and ATT using the data from Morgan & Winship 
-#       book on causal models, chapters 5 and 7
+# Date: 06/09/2021
+# Desc: example for matching demonstration 1 P145 Morgan & Winship 
+#       book on causal models
 
-# data downloaded from https://www.cambridge.org/gb/academic/subjects/sociology/sociology-general-interest/counterfactuals-and-causal-inference-methods-and-principles-social-research-2nd-edition?format=HB&isbn=9781107065079
+# simulate the data for matching demonstration 1 page 145
 
-# import and explore the data
-library(foreign)
-dfData = read.dta(file.choose())
-
-# simulate the data for matching demonstration 3 page 153
-# and page 229
 
 # propensity score function
-# score = function(A, B) {
-#   r = -2 +3*(A) -3*(A-0.1) +2*(A-.3) -2*(A-.5) +4*(A-.7)
-#   -4*(A-.9) +1*(B) -1*(B-.1) +2*(B-.7) -2*(B -.9) +3*(A-.5)*(B -.5)
-#   -3*(A-.7)*(B -.7)
-#   return(plogis(r))
-# }
-
-score = function(A, B){
-  return(rbeta(length(A), 10, 10))
+score = function(S){
+  if (S == 1) {
+    return(0.18)
+  }
+  else if (S == 2) {
+    return(0.5)
+  } else if (S == 3){
+    return(0.625)
+  }
+  else {stop('unknown value')}
 }
 
-
-# create grid of points
-A = seq(0.1, 1, length.out=30)
-B = seq(0.1, 1, length.out = 30)
-
-z = outer(A, B, score)
-persp(A, B, z, theta = -30, phi=30, col='green', 
-      ticktype = 'detailed', zlim = c(0, 1), cex.axis=0.8)
-# generate 2 random variables
+# generate a data vector
 set.seed(123)
-A = round(runif(100, 0.1, 1), 1)
-B = round(runif(100, 0.1, 1), 1)
+S = sample(c(1, 2, 3), size = 100, replace = T, prob = c(0.44, 0.24, 0.32))
 
-y1 = 102 + 6*A + 4*B  + rnorm(100, 0, 5)
-y0 = 100 + 3*A + 2*B  + rnorm(100, 0, 5)
+# counter-factual data
+y_0 = function(S){
+  if (S == 1) {
+    return(2)
+  }
+  else if (S == 2) {
+    return(6)
+  } else if (S == 3){
+    return(10)
+  }
+  else {stop('unknown value')}
+}
 
-ps = score(A, B)
+y_1 = function(S){
+  if (S == 1) {
+    return(4)
+  }
+  else if (S == 2) {
+    return(8)
+  } else if (S == 3){
+    return(14)
+  }
+  else {stop('unknown value')}
+}
+
+y1 = sapply(S, y_1) 
+y0 = sapply(S, y_0)
+
+## select treatment variable by using propensity score
+## more S3 have been assigned treatment i.e P(D=1 | S=3) = 0.625 
+ps = sapply(S, score)
 d = rbinom(100, 1, ps)
-
+table(S, d)
+# generate observed data
 y = y1*(d) + (1-d)*y0
 
-dfData = data.frame(y, d=factor(d), A, B, y1, y0, p=ps)
-mean(y1 - y0)
+mean(y[d == 1])
+mean(y[d == 0])
+# naive estimator E[y | d = 1] - E[y | d = 0]
+mean(y[d == 1]) - mean(y[d == 0])
+
+## the same result after weighting the data by the
+## conditional factor S
+# under control state after weighting for conditional factor S 
+# P(S=1 | D=0) * Y etc
+0.6*2 + 0.2*6 + 0.2*10 # E(Y0|D=0) (after marginalising S)
+# under treatment state 
+# P(S=1| D=1) * Y etc
+0.2*4 + 0.3*8 + 0.5*14 # E(Y1|D=1)
+# naive estimator 
+10.2 - 4.4
+
+## now weight the differences between D=1 and D=0
+## ATT
+mean(y[d==1 & S==1])*0.2 - mean(y[d==0 & S==1]) * 0.2 # P(S=1|D=1)=P(S,D)/P(D) = 0.08/0.4
+mean(y[d==1 & S==2])*0.3 - mean(y[d==0 & S==2]) * 0.3 # P(S=2|D=1)
+mean(y[d==1 & S==3])*0.5 - mean(y[d==0 & S==3]) * 0.5 # P(S=3|D=1)
+0.4 + 0.6 + 2
+
+## ATC
+mean(y[d==1 & S==1])*0.6 - mean(y[d==0 & S==1]) * 0.6 # P(S=1|D=0)
+mean(y[d==1 & S==2])*0.2 - mean(y[d==0 & S==2]) * 0.2 # P(S=2|D=0)
+mean(y[d==1 & S==3])*0.2 - mean(y[d==0 & S==3]) * 0.2 # P(S=3|D=0)
+1.2 + 0.4 + 0.8
+
+## ATE
+mean(y[d==1 & S==1])*0.44 - mean(y[d==0 & S==1]) * 0.44 # P(S=1)
+mean(y[d==1 & S==2])*0.24 - mean(y[d==0 & S==2]) * 0.24 # P(S=2)
+mean(y[d==1 & S==3])*0.32 - mean(y[d==0 & S==3]) * 0.32 # P(S=3)
+0.88 + 0.48 + 1.28
+## or weight the ATT and ATC by P(D)
+3*0.4 + 2.4*0.6 
+
+## try regression technique
+dfData = data.frame(y, d=factor(d), S=factor(S), y1, y0, p=ps)
+summary(dfData)
 
 f1 = lm(y ~ d, data=dfData)
 summary(f1)
 
-f2 = lm(y ~ d + A + B, data=dfData)
+f2 = lm(y ~ d + S, data=dfData)
 summary(f2)
 
-f3 = lm(y ~ d, weights = p, data=dfData)
+f3 = lm(y ~ d + S, data=dfData, weights=1-p)
 summary(f3)
 
-f4 = lm(y ~ d + A + B, weights=p, data=dfData)
+f4 = lm(y ~ d + S, data=dfData, weights=p)
 summary(f4)
 
 library(arm)
@@ -70,14 +122,18 @@ matched = as.logical(matches$cnts)
 #rownames(dfData) = 1:nrow(dfData)
 dfData.matched = dfData[matched,]
 dim(dfData.matched)
+
 f5 = lm(y ~ d, data=dfData.matched)
 summary(f5)
 
-f6 = lm(y ~ d + A + B, data=dfData.matched)
+f6 = lm(y ~ d + S, data=dfData.matched)
 summary(f6)
 
 f7 = lm(y ~ d, weights = p, data=dfData.matched)
 summary(f7)
 
-f8 = lm(y ~ d + A + B, weights=p, data=dfData.matched)
+f8 = lm(y ~ d + S, weights=p, data=dfData.matched)
+summary(f8)
+
+f8 = lm(y ~ d + S, weights=1-p, data=dfData.matched)
 summary(f8)
